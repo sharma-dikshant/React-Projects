6b1aec1ecd414576ca64a9565776cef2ebbac6b2
sharma-dikshant <dikshantsharma2005@gmail.com>
2024-07-24 22:53:14 +0530

    Accodion: version 2 completed

--- src/components/accordion/accordion.jsx
+++ src/components/accordion/accordion.jsx
@@ -3,27 +3,38 @@ import accordionData from "../../assets/accodionData";
 import "./accordion.css";
 
 export default function Accordion() {
+  const [selectedQues, setSelectedQues] = useState(null);
+
+  function handleSelectQues(id) {
+    setSelectedQues((prev) => (prev !== id ? id : null));
+  }
+
   return (
     <ul className="accodion-container">
       {accordionData.map((question) => (
-        <Question question={question} key={question.id} />
+        <Question
+          question={question}
+          key={question.id}
+          selectedQues={selectedQues}
+          onSelectQues={handleSelectQues}
+        />
       ))}
     </ul>
   );
 }
 
-function Question({ question }) {
+function Question({ question, selectedQues, onSelectQues }) {
   const [isSelected, setIsSelected] = useState(false);
   return (
     <li className="accordion-item">
       <div
         style={{ display: "flex", justifyContent: "space-between" }}
-        onClick={() => setIsSelected(!isSelected)}
+        onClick={(evt) => onSelectQues(question.id)}
       >
         <p>{question.question}</p>
         <p>+</p>
       </div>
-      {isSelected ? <div>{question.answer}</div> : null}
+      {selectedQues === question.id ? <div>{question.answer}</div> : null}
     </li>
   );
 }
